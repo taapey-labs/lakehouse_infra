1e38e1ea6b2887c1ed110f9c4ac5cc708814a791
Nepal Singh <nepal.singh@gmail.com>
2026-09-10 12:52:43 -0700

    updated

--- tf/main.tf
+++ tf/main.tf
@@ -19,9 +19,10 @@ module "databricks_sra" {
 
   custom_vpc_id                 = var.custom_vpc_id
   custom_private_subnet_ids     = var.custom_private_subnet_ids
-  # custom_general_access_vpce_id = var.custom_general_access_vpce_id
-  # custom_scc_relay_vpce_id      = var.custom_scc_relay_vpce_id
+  custom_general_access_vpce_id = var.custom_general_access_vpce_id
+  custom_scc_relay_vpce_id      = var.custom_scc_relay_vpce_id
   sg_egress_ports = var.sg_egress_ports
+  custom_sg_id                  = var.custom_sg_id
 
   databricks_client_id     = var.databricks_client_id
   databricks_client_secret = var.databricks_client_secret
